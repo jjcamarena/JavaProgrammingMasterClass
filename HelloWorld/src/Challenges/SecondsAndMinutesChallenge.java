421979b618b3e9efa31ad581ab3786d8a86d21ca
Pepe Camarena <jjcamarena@gmail.com>
2023-11-18 00:47:55 -0600

    Initial commit

---
+++ HelloWorld/src/Challenges/SecondsAndMinutesChallenge.java
@@ -0,0 +1,17 @@
+package Challenges;
+
+public class SecondsAndMinutesChallenge {
+    public static void main(String[] args) {
+        // XXh YYm ZZs
+        // seconds >= 0
+    }
+
+    public static void getDurationString(int seconds) {
+        //
+    }
+
+    public static void getDurationString(int minutes, int seconds) {
+
+    }
+
+}
