bfd41d183ad190446c4289f0d21f76de2fbea2ee
Pepe Camarena <jjcamarena@gmail.com>
2023-11-29 02:09:37 -0600

    SecondsAndMinutesChallenge v2

--- HelloWorld/src/Challenges/SecondsAndMinutesChallenge.java
+++ HelloWorld/src/Challenges/SecondsAndMinutesChallenge.java
@@ -4,25 +4,31 @@ public class SecondsAndMinutesChallenge {
     public static void main(String[] args) {
         // XXh YYm ZZs
         // seconds >= 0
-        getDurationString(3945);
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(-65, 45));
+        System.out.println(getDurationString(65, 145));
     }
 
-    public static void getDurationString(int seconds) {
-        //
-        if (seconds <= 0) {
-            System.out.println("Entered value most be greater than zero");
+    public static String getDurationString(int seconds) {
+        int minutes = 0;
+        if (seconds < 0) {
+            return "Entered value most be greater than zero";
         } else {
-            int minutes = seconds / 60;
+            minutes = seconds / 60;
             seconds = seconds % 60;
-            // System.out.println("minutes = " + minutes);
-            // System.out.println("seconds = " + seconds);
-            System.out.println(getDurationString(minutes, (seconds % 60)));
         }
+        return getDurationString(minutes, (seconds % 60));
     }
 
     public static String getDurationString(int minutes, int seconds) {
-        int hours = minutes / 60;
-        minutes = (minutes % 60);
+        int hours = 0;
+        if (minutes < 0 || seconds < 0) {
+            return "Entered value most be greater than zero";
+        } else {
+            hours = minutes / 60;
+            minutes = (minutes % 60) + seconds / 60;
+            seconds = seconds % 60;
+        }
         return hours + "h " + minutes + "m " + seconds + "s";
     }
 
